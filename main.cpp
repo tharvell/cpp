47168a6081f08a18f85b25eaf71c57e55d8c81e9
tharvell <harvellt@gmail.com>
2023-08-27 02:27:32 -0400

    Formatting

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
-int main(){
-    return 0;
+#include <SFML/Window.hpp>
+
+int main() {
+  sf::Window window(sf::VideoMode(800, 600), "My window");
+
+  // run the program as long as the window is open
+  while (window.isOpen()) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+      window.close();
+    }
+    // check all the window's events that were triggered since the last
+    // iteration of the loop
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      // "close requested" event: we close the window
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+  }
+
+  return 0;
 }
